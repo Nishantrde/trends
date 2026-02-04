cb78c0d669c184b5253a28dfabe96ece1cd78a10
Nishant <nishant2024rde@gmail.com>
2026-02-04 21:14:56 +0530

    world is seen

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,6 @@ function update(geojson: any, geoGenerator: any) {
 
 export default function Home(){
   useEffect(() => {
-    const geojson = json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json')
-    .then((geojson: any) => {
-        update(geojson, geoGenerator);
-      });
     const width = window.innerWidth;
     const height = window.innerHeight;
 
@@ -31,7 +27,10 @@ export default function Home(){
     const geoGenerator = geoPath()
       .projection(projection);
 
-    update(geojson, geoGenerator);
+    json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json')
+      .then((geojson: any) => {
+        update(geojson, geoGenerator);
+      });
   }, []);
 
   return(
