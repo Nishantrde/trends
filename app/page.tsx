52e07303d46f0c63d9c74db2252a2b630d5d2637
Nishant <nishant2024rde@gmail.com>
2026-02-04 20:44:03 +0530

    adding canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+import { select } from 'https://esm.sh/d3-selection';
+import { geoPath, geoEquirectangular } from 'https://esm.sh/d3-geo';
+
+let geojson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Africa"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-6, 36], [33, 30], [43, 11], [51, 12], [29, -33], [18, -35], [7, 5], [-17, 14], [-6, 36]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Australia"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[143, -11], [153, -28], [144, -38], [131, -31], [116, -35], [114, -22], [136, -12], [140, -17], [143, -11]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Timbuktu"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-3.0026, 16.7666]
+      }
+    }
+  ]
+};
+
+
 export default function Home(){
   return(
     <div id="content">
     <svg width="800px" height="400px">
-      <g class="map"></g>
+      <g className="map"></g>
     </svg>
   </div>
   )
